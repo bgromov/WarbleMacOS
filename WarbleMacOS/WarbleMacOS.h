3932a9ec76315cafb6100a43bb204df03ce94b8c
Boris Gromov <0xff.root@gmail.com>
2018-08-19 14:27:50 +0200

    Initial Commit

---
+++ WarbleMacOS/WarbleMacOS.h
@@ -0,0 +1,19 @@
+//
+//  WarbleMacOS.h
+//  WarbleMacOS
+//
+//  Created by 0xff on 19/08/2018.
+//  Copyright © 2018 0xff. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for WarbleMacOS.
+FOUNDATION_EXPORT double WarbleMacOSVersionNumber;
+
+//! Project version string for WarbleMacOS.
+FOUNDATION_EXPORT const unsigned char WarbleMacOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WarbleMacOS/PublicHeader.h>
+
+
